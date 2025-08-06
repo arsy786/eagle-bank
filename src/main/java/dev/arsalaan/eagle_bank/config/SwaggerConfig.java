988ce6382119065911119a846d621b53be0660be
arsy786 <arsalaannaeem@hotmail.com>
2025-08-06 15:27:29 +0100

    swagger config for JWT

--- src/main/java/dev/arsalaan/eagle_bank/config/SwaggerConfig.java
+++ src/main/java/dev/arsalaan/eagle_bank/config/SwaggerConfig.java
@@ -2,11 +2,16 @@ package dev.arsalaan.eagle_bank.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+  private static final String SECURITY_SCHEME_NAME = "bearerToken";
 
   @Bean
   public OpenAPI customOpenAPI() {
@@ -14,7 +19,14 @@ public class SwaggerConfig {
         .info(new Info()
             .title("Eagle Bank API")
             .version("1.0")
-            .description("REST API documentation for Eagle Bank"));
+            .description("REST API documentation for Eagle Bank"))
+        .components(new Components()
+            .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                new SecurityScheme()
+                    .name("Authorization")
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
+        .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME));
   }
-
 }
